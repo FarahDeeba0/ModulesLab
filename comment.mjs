da9439aa06ab1627cfd50a8bd83e0af88048a1c6
FarahDeeba0 <160637165+FarahDeeba0@users.noreply.github.com>
2024-04-05 03:14:57 -0400

    first commit

---
+++ comment.mjs
@@ -0,0 +1,20 @@
+//fetch request -> JSON placeholder
+//async helps things run in the background
+//fetch API to fetch things--u need http method and url.. if not specified the default is get(get comments as data)
+export async function getComments() {
+    fetch("https://jsonplaceholder.typicode.com/comments")
+        .then((response) => {
+            return response.json()
+        
+            // console.log(response)
+        })
+        .then((data) => {
+            console.log(data)
+            for (let i = 0; i < 20; i++) {
+                let commentElement = document.createElement('p');
+                commentElement.innerHTML = data[i].body;
+                document.body.appendChild(commentElement);
+
+            }
+        });
+}
